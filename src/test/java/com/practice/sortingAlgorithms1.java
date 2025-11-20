699c8b5cb07c7c194c506d19b4b5771bec5c7e33
Sathvik PM <SATHVIKPAAROL2002@GMAIL.COM>
2025-11-20 08:54:43 +0530

    practced bubbleSort and insertionSort

--- src/test/java/com/practice/sortingAlgorithms1.java
+++ src/test/java/com/practice/sortingAlgorithms1.java
@@ -5,6 +5,7 @@ public class sortingAlgorithms1 {
     public static void main(String[] args) {
         int[] arr={2,4,5,7,8};
         selectionSort(arr);
+        bubbleSort(arr);
 
     }
 
@@ -32,5 +33,43 @@ public class sortingAlgorithms1 {
             arr[mini]=temp;
         }
 
+    }
+
+    /*
+    Given an array, arr[]. Sort the array using bubble sort algorithm.
+    Examples :
+
+    Input: arr[] = [4, 1, 3, 9, 7]
+    Output: [1, 3, 4, 7, 9]
+    Explanation: After Sorting the array in ascending order of their values is [1, 3, 4, 7, 9].
+     */
+    static void bubbleSort(int[] arr) {
+        // code here
+            // code here
+            for(int i=0;i<arr.length-1;i++){
+                int swap=0;
+                for(int j=0;j<arr.length-i-1;j++){
+                    if(arr[j]>arr[j+1]){
+                        swap++;
+                        int temp=arr[j];
+                        arr[j]=arr[j+1];
+                        arr[j+1]=temp;
+                    }
+                }
+                if(swap==0){
+                    return;
+                }
+        }
+    }
+    static void insertionSort(int arr[]) {
+        for(int i=0;i<arr.length;i++){
+            int j=i;
+            while(j>0 && arr[j-1]>arr[j]){
+                int temp=arr[j];
+                arr[j]=arr[j-1];
+                arr[j-1]=temp;
+                j--;
+            }
+        }
     }
 }
